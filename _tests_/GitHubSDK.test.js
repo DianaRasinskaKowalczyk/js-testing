0d70d963588f06d82cf9b18f7b612285e31ffc3f
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-04-07 17:24:12 +0200

    Try to make tests but they don't work

--- _tests_/GitHubSDK.test.js
+++ _tests_/GitHubSDK.test.js
@@ -1,18 +1,74 @@
 import nodeFetch from "node-fetch";
 global.fetch = nodeFetch;
-import { token } from "../token.js";
+// import token from "../token.js";
 import GitHubSDK from "../src/GitHubSDK.js";
 
 describe("class GitHubSDK", () => {
 	describe(".getUser()", () => {
 		it("should return a correct login when correct user is provided", () => {
 			expect.assertions(1);
-			const gh = new GitHubSDK(token);
+			const gh = new GitHubSDK();
 			const userName = "DianaRasinskaKowalczyk";
 			const promise = gh.getUser(userName);
 
 			return promise.then(result => {
 				expect(result.login).toBe(userName);
+			});
+		});
+
+		it("Should reject with status code 404 when incorrect username provided", () => {
+			expect.assertions(1);
+			const gh = new GitHubSDK();
+			const username = "wrong-username-which-is-unique";
+
+			const promise = gh.getUser(username);
+
+			return promise.catch(error => {
+				expect(error.status).toBe(404);
+			});
+		});
+	});
+
+	describe(".getRepos()", () => {
+		it("should return a list of repositories if a username exists", () => {
+			expect.assertions(1);
+			const gh = GitHubSDK();
+			const userName = "DianaRasinskaKowalczyk";
+
+			const promise = gh.getRepos(userName);
+
+			return promise.then(result => {
+				expect(Array.isArray(result)).toBeTruthy();
+			});
+		});
+	});
+
+	describe(".getRepo()", () => {
+		it("should return repo's name if user and repo exist", () => {
+			expect.assertions(1);
+			const gh = GitHubSDK();
+			const userName = "DianaRasinskaKowalczyk";
+			const repo = "task-js-basics";
+
+			const promise = gh.getRepo(userName, repo);
+
+			return promise.then(result => {
+				expect(result).toBe(repo);
+			});
+		});
+	});
+
+	describe(".sendInvitation()", () => {
+		it("should return true if a user exists", () => {
+			expect.assertions(1);
+			const gh = GitHubSDK("devmentor-pl");
+			const userName = "DianaRasinskaKowalczyk";
+			const repo = "task-js-basics";
+
+			const promise = gh.sendInvitation(repo, userName);
+
+			return promise.then(result => {
+				expect(result).toBeTruthy;
 			});
 		});
 	});
--- src/GitHubSDK.js
+++ src/GitHubSDK.js
@@ -1,18 +1,60 @@
 export default class GitHubSDK {
-	constructor(token) {
+	constructor(token, owner) {
 		this.token = token;
 		this.apiLink = "https://api.github.com";
+		this.owner = owner;
 	}
 
 	getUser(userName) {
 		const options = {
 			headers: {
 				Accept: "application/vnd.github+json",
-				Authorization: `token ${this.token}`,
+				// Authorization: `token ${this.token}`,
 			},
 		};
 
 		return this._fetch(options, `/users/${userName}`);
+	}
+
+	getRepos(userName) {
+		const options = {
+			headers: {
+				Accept: "application/vnd.github+json",
+				// Authorization: `token ${this.token}`,
+			},
+		};
+
+		return this._fetch(options, `/orgs/${userName}/repos`);
+	}
+
+	getRepo(userName, repo) {
+		const options = {
+			headers: {
+				Accept: "application/vnd.github+json",
+				// Authorization: `token ${this.token}`,
+			},
+		};
+
+		return this._fetch(options, `/repos/${userName}/${repo}`);
+	}
+
+	sendInvitation(repo, name) {
+		const options = {
+			method: "PUT",
+			credentials: "same-origin",
+			redirect: "follow",
+			headers: {
+				Accept: "application/vnd.github.v3+json",
+				Authorization: `token ${token}`,
+			},
+			body: JSON.stringify({
+				permission: "pull",
+			}),
+		};
+		return this._fetch(
+			options,
+			`/repos/${this.owner}/${repo}/collaborators/${name}`
+		);
 	}
 
 	_fetch(options, additionalPath = "") {
